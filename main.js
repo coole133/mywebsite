ac5750c15def04ce5ac12dbd7b576ca8f2757735
coole133 <vlad.yaromiy@gmail.com>
2020-02-22 16:00:19 +0200

    version 1.1

---
+++ main.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+  $(".grid-container").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: $(".arrow__left"),
+    nextArrow: $(".arrow__right"),
+    speed: 1000,
+    easing: "easeOutExpo",
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function() {
+  $(".manageDisplay").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    prevArrow: $(".arrow__container"),
+    nextArrow: $(".arrow__container--right")
+  });
+});
+
+$(".hamburger").on("click", function() {
+  $(".hamburger").toggleClass("cross");
+  $(".menu-btn").toggleClass("menu-active");
+});
+
+$(".manage-icon").on("click", function() {
+  $(".manage-icon").toggleClass("cross");
+  $(".shop-btn").toggleClass("shop-active");
+});
+
+$(window).scroll(function() {
+  $(".navigation").toggleClass("scrolled", $(this).scrollTop() > 850);
+});
+
+let question = document.querySelectorAll(".question");
+
+function activation(element) {
+  question.forEach(item => {
+    item.classList.remove("is-activated");
+  });
+
+  element.classList.add("is-activated");
+}
+
+const questionsContainer = document.querySelector(".questions");
+
+questionsContainer.addEventListener("click", e => {
+  const target = e.target;
+  const parentNode = target.parentNode;
+
+  if (
+    target.classList.contains("tab") &&
+    !parentNode.classList.contains("is-activated")
+  ) {
+    activation(parentNode);
+  }
+});
+
+// Так більше не робити!
+// question.forEach(item => {
+//   item.addEventListener("click", activation);
+// });
+
+let tabs = document.querySelectorAll(".tab-name");
+let tabContent = document.querySelectorAll(".tab-content");
+let tabId;
+
+function highlite() {
+  tabs.forEach(item => {
+    item.classList.remove("is-highlated");
+  });
+  this.classList.add("is-highlated");
+  tabId = this.getAttribute("data-tab-name");
+  selectTabContent(tabId);
+}
+
+function selectTabContent(tabId) {
+  tabContent.forEach(item => {
+    item.classList.contains(tabId)
+      ? item.classList.add("changed")
+      : item.classList.remove("changed");
+  });
+}
+
+tabs.forEach(item => {
+  item.addEventListener("click", highlite);
+});
